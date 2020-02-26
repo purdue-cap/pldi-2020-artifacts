f51cc0fa99bfd45e203b781a06a888b0ab3c4836
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-02-26 04:41:04 -0500

    Change LD_LIBRARY_PATH to append

--- scripts/build_dryadsynth.sh
+++ scripts/build_dryadsynth.sh
@@ -5,4 +5,4 @@ set -e
 cd /home/user/ && git clone https://github.com/purdue-cap/DryadSynth
 cd DryadSynth && cp -f /usr/local/lib/com.microsoft.z3.jar lib/
 make
-echo 'export LD_LIBRARY_PATH=/usr/local/lib' >> /home/user/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib' >> /home/user/.bashrc
