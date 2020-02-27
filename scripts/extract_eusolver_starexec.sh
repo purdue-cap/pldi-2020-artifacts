ebb64809945f00ce6219a6d9ffe73fc3955f4405
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-02-26 22:41:14 -0500

    Fix EUSolver execution script error

--- scripts/extract_eusolver_starexec.sh
+++ scripts/extract_eusolver_starexec.sh
@@ -6,6 +6,7 @@ mv EUSolver-2018 EUSolver
 
 chmod +x EUSolver/thirdparty/Python-3.5.1/python3
 cat > EUSolver/bin/eusolver.sh << 'EOF'
+#!/bin/bash
 BASE=$(dirname $0)
 BENCH=$(realpath $1)
 cd $BASE && exec ./starexec_run_python3 $BENCH
