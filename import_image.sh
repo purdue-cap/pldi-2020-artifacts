ae1fe46b5fadac6de57a0cc50657c550ff2fae9d
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-03-09 22:03:29 -0400

    Improved import_image.sh script

--- import_image.sh
+++ import_image.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -e
 
-if md5sum -c dryadsynth.tar.xz.md5; then
-    xz -d -c dryadsynth.tar.xz | docker load
+if command -v md5sum >/dev/null 2>&1; then
+    if md5sum -c dryadsynth.tar.xz.md5; then
+        xz -d -c dryadsynth.tar.xz | docker load
+    else
+        echo 'Checksum mismatch, file may be corrupted!'
+        exit 1
+    fi
 else
-    echo 'Checksum mismatch, file may be corrupted!'
-    exit 1
+    echo 'md5sum command not found on your system'
+    echo 'Please manually check md5 of dryadsynth.tar.xz to be matching:'
+    cat dryadsynth.tar.xz.md5
+    echo 'Press enter to continue loading the image...'
+    read
+    xz -d -c dryadsynth.tar.xz | docker load
 fi
+
